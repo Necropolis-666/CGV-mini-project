61be6c90d5ea8c223b6a3962d1d86fcc2f6740d3
Axinion <55135463+Axinion@users.noreply.github.com>
2021-11-16 08:14:41 +0530

    Moving of the car
    It includes:
    Drawing the images on the screen
    Scaling the image
    Rotation of the car
    Acceleration and deacceleration of car

---
+++ utils.py
@@ -0,0 +1,13 @@
+import pygame
+
+# function that scales the image
+def scale_image(img, factor):
+    size = round(img.get_width() * factor), round(img.get_height() * factor)
+    return pygame.transform.scale(img, size)
+
+# function that takes a image and returns a rotated image based on an angle
+def blit_rotate_center(win, image, top_left, angle):
+    rotated_image = pygame.transform.rotate(image, angle)
+    new_rect = rotated_image.get_rect(
+        center=image.get_rect(topleft=top_left).center)
+    win.blit(rotated_image, new_rect.topleft)
